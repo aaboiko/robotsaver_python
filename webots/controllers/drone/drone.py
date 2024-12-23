c40394076b828f43419dd32f55e522f9f5becbdc
aaboiko <aboiko.exceedteam@gmail.com>
2024-12-23 20:16:20 +0300

    robot is moving correctly

--- webots/controllers/drone/drone.py
+++ webots/controllers/drone/drone.py
@@ -2,54 +2,19 @@ import numpy as np
 import cv2
 import math
 import matplotlib.pyplot as plt
+import struct
+
+from ultralytics import YOLO
 
 from controller import Robot, Supervisor
 from controller import Camera
-from controller import Compass
-from controller import GPS
-from controller import Gyro
-from controller import InertialUnit
 from controller import Keyboard
-from controller import LED
 from controller import Motor
+from controller import Emitter, Receiver
 
+from controllers.robot.robot import handler
 
-k_vertical_thrust = 68.5
-k_vertical_offset = 0.6
-k_vertical_p = 3.0
-k_roll_p = 50.0
-k_pitch_p = 30.0
-
-target_altitude = 1.0
-
-
-def CLAMP(value, low, high):
-    if value < low:
-        return low
-    else:
-        if value > high:
-            return high
-        else:
-            return value
-        
-
-def get_control_inputs(roll_disturbance, pitch_disturbance, yaw_disturbance, target_altitude, gps_values, gyro_values, imu_values):
-    altitude = gps_values[2]
-    roll_velocity, pitch_velocity, yaw_velocity = gyro_values
-    roll, pitch, yaw = imu_values
-
-    roll_input = k_roll_p * CLAMP(roll, -1.0, 1.0) + roll_velocity + roll_disturbance
-    pitch_input = k_pitch_p * CLAMP(pitch, -1.0, 1.0) + pitch_velocity + pitch_disturbance
-    yaw_input = yaw_disturbance
-    clamped_difference_altitude = CLAMP(target_altitude - altitude + k_vertical_offset, -1.0, 1.0)
-    vertical_input = k_vertical_p * clamped_difference_altitude**3
-
-    front_left_motor_input = k_vertical_thrust + vertical_input - roll_input + pitch_input - yaw_input
-    front_right_motor_input = k_vertical_thrust + vertical_input + roll_input + pitch_input + yaw_input
-    rear_left_motor_input = k_vertical_thrust + vertical_input - roll_input - pitch_input + yaw_input
-    rear_right_motor_input = k_vertical_thrust + vertical_input + roll_input - pitch_input - yaw_input
-
-    return [front_left_motor_input, front_right_motor_input, rear_left_motor_input, rear_right_motor_input]
+model_name = "yolov8n"
         
 
 def main():
@@ -60,63 +25,52 @@ def main():
 
     camera = robot.getDevice('camera')
     camera.enable(timestep)
-    front_left_led = robot.getDevice("front left led")
-    front_right_led = robot.getDevice("front right led")
-    imu = robot.getDevice("inertial unit")
-    imu.enable(timestep)
-    gps = robot.getDevice("gps")
-    gps.enable(timestep)
-    compass = robot.getDevice("compass")
-    compass.enable(timestep)
-    gyro = robot.getDevice("gyro")
-    gyro.enable(timestep)
 
     camera_roll_motor = robot.getDevice("camera roll")
     camera_pitch_motor = robot.getDevice("camera pitch")
+    camera_yaw_motor = robot.getDevice("camera yaw")
 
-    front_left_motor = robot.getDevice("front left propeller")
-    front_right_motor = robot.getDevice("front right propeller")
-    rear_left_motor = robot.getDevice("rear left propeller")
-    rear_right_motor = robot.getDevice("rear right propeller")
-    motors = [front_left_motor, front_right_motor, rear_left_motor, rear_right_motor]
+    print("Start the camera post...\n")
 
-    for motor in motors:
-        motor.setPosition(np.inf)
-        motor.setVelocity(1.0)
+    model = YOLO(model_name)
+    emitter = robot.getDevice("emitter")
 
-    print("Start the drone...\n")
+    keyboard = Keyboard()
+    keyboard.enable(timestep)
 
-    while robot.step(timestep) != -1:
-        if robot.getTime() > 1.0:
-            break
+    camera_yaw = 0
+    camera_pitch = 0
 
     while robot.step(timestep) != -1:
         time = robot.getTime()
 
-        imu_values = imu.getRollPitchYaw()
-        roll, pitch, yaw = imu_values
-        gps_values = gps.getValues()
-        altitude = gps_values[2]
-        gyro_values = gyro.getValues()
-        roll_velocity, pitch_velocity, yaw_velocity = gyro_values
+        image = camera.getImage()
+        image_rgba = np.frombuffer(image, np.uint8).reshape((camera.getHeight(), camera.getWidth(), 4)) 
+        image_rgb = cv2.cvtColor(image_rgba, cv2.COLOR_RGBA2RGB)
 
-        led_state = int(time) % 2
-        front_left_led.set(led_state)
-        front_right_led.set(1 - led_state)
+        results = model(image_rgb)
 
-        camera_roll_motor.setPosition(-0.115 * roll_velocity)
-        camera_pitch_motor.setPosition(-0.1 * pitch_velocity)
+        data = struct.pack(">f", 1.0)
+        emitter.send(data)
+        #print('data sent: ' + data)
+        handler.inc()
 
-        roll_disturbance = 0.0
-        pitch_disturbance = 0.0
-        yaw_disturbance = 0.0
+        #camera_roll_motor.setPosition(0)
+        #camera_pitch_motor.setPosition(0)
 
-        front_left_motor_input, front_right_motor_input, rear_left_motor_input, rear_right_motor_input = get_control_inputs(roll_disturbance, pitch_disturbance, yaw_disturbance, target_altitude, gps_values, gyro_values, imu_values)
+        key = keyboard.getKey()
 
-        front_left_motor.setVelocity(front_left_motor_input)
-        front_right_motor.setVelocity(-front_right_motor_input)
-        rear_left_motor.setVelocity(-rear_left_motor_input)
-        rear_right_motor.setVelocity(rear_right_motor_input)
+        if key > 0:
+            if key == Keyboard.UP:
+                camera_pitch += 0.01
+            elif key == Keyboard.DOWN:
+                camera_pitch -= 0.01
+            elif key == Keyboard.LEFT:
+                camera_yaw += 0.01
+            elif key == Keyboard.RIGHT:
+                camera_yaw -= 0.01
 
+        camera_yaw_motor.setPosition(camera_yaw)
+        camera_pitch_motor.setPosition(camera_pitch)
 
 main()
--- webots/controllers/robot/robot.py
+++ webots/controllers/robot/robot.py
@@ -1,11 +1,22 @@
 import numpy as np
 import cv2
 import math
+import struct
 
 from controller import Robot, Supervisor
 from controller import Gyro
 from controller import InertialUnit
 from controller import Motor
+from controller import Emitter, Receiver
+
+class Handler:
+    def __init__(self):
+        self.value = 0
+
+    def inc(self):
+        self.value += 1
+
+handler = Handler()
 
 def main():
     robot = Supervisor()
@@ -16,9 +27,16 @@ def main():
     gyro = robot.getDevice("gyro")
     gyro.enable(timestep)
 
-    motor = robot.getDevice("robot_left_motor")
-    motor.setPosition(np.inf)
-    motor.setVelocity(1.0)
+    receiver = robot.getDevice("receiver")
+    receiver.enable(timestep)
+
+    motor_left = robot.getDevice("robot_left_motor")
+    motor_right = robot.getDevice("robot_right_motor")
+
+    motor_left.setPosition(np.inf)
+    motor_left.setVelocity(0.0)
+    motor_right.setPosition(np.inf)
+    motor_right.setVelocity(0.0)
 
     while robot.step(timestep) != -1:
         if robot.getTime() > 1.0:
@@ -32,7 +50,16 @@ def main():
         gyro_values = gyro.getValues()
         roll_velocity, pitch_velocity, yaw_velocity = gyro_values
 
-        motor.setVelocity(2.0)
+        motor_left.setVelocity(5.0)
+        motor_right.setVelocity(5.0)
+
+        #print('queue length: ' + str(receiver.getQueueLength()))
+        print("value: " + str(handler.value))
+
+        '''while receiver.getQueueLength() > 0:
+            data = receiver.getFloats()
+            print('data received: ' + str(data))
+            receiver.nextPacket()'''
 
 
 main()
